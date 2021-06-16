05eea5093d86a2eb993ce9a28d7ded07b1c75e09
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2021-06-16 11:58:51 +0300

    Реализует hide

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ item.addEventListener('dragend', onDragEnd)
 
 function onDragStart() {
     item.classList.add('hold');
+    setTimeout(()=>{
+        item.classList.add('hide');
+    },0)
 }
 
 function onDragEnd() {
-    item.classList.remove('hold');
+    item.classList.remove('hold', 'hide');
 }
