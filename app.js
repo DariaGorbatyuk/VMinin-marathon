3efb9faec5a421c146ffa271273575ad428b80f4
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2021-06-16 12:10:51 +0300

    Добавляет основные listeners

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 const item = document.querySelector('.item');
+const columns = document.querySelectorAll('.placeholder');
+item.addEventListener('dragstart', onDragStart);
+item.addEventListener('dragend', onDragEnd);
 
-item.addEventListener('dragstart', onDragStart)
-item.addEventListener('dragend', onDragEnd)
+for (const column of columns) {
+    column.addEventListener('dragover', onDragOver);
+    column.addEventListener('dragenter', onDragEnter);
+    column.addEventListener('dragleave', onDragLeave);
+    column.addEventListener('drop', onDragDrop);
+}
 
 function onDragStart() {
     item.classList.add('hold');
-    setTimeout(()=>{
+    setTimeout(() => {
         item.classList.add('hide');
-    },0)
+    }, 0)
 }
 
 function onDragEnd() {
     item.classList.remove('hold', 'hide');
+}
+
+function onDragOver(evt) {
+    evt.preventDefault();
+}
+
+function onDragEnter(evt){
+    evt.currentTarget.classList.add('enter');
+}
+function onDragLeave(evt){
+    evt.currentTarget.classList.remove('enter');
+}
+function onDragDrop(evt){
+    evt.currentTarget.classList.remove('enter');
+
 }
